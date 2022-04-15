252c5a48d05997daaf58ac119585ec32d4c897a7
WML <menglinw@usc.edu>
2022-04-14 18:41:55 -0700

    add alpha, changing to meta-SGD

--- probdownscale/meta_train/MAML.py
+++ probdownscale/meta_train/MAML.py
@@ -12,9 +12,13 @@ class MAML:
         '''
         self.meta_model = target_model
         self.loss = target_loss
+        self.alpha = tf.Variable(tf.random_normal_initializer(shape=target_model.get_weights().shape,
+
+                                                              dtype=tf.float64))
 
     def train_on_batch(self, train_data, inner_optimizer, inner_step, outer_optimizer=None):
         """
+
         :param train_data: training data for one batch, including meta_train_X, meta_train_Y, meta_test_X, meta_test_Y
         :param inner_optimizer: optimizer for inner loop
         :param inner_step: steps of learning for inner loop
@@ -27,6 +31,7 @@ class MAML:
 
         # save the weight as initialization
         meta_weights = self.meta_model.get_weights()
+        alpha = self.alpha
 
         meta_train_X, meta_train_Y, meta_test_X, meta_test_Y = train_data
         for train_X, train_Y in zip(meta_train_X, meta_train_Y):
@@ -40,27 +45,33 @@ class MAML:
                     loss = self.loss(train_Y, Y_hat)
                     loss = tf.reduce_mean(loss)
                 grads = tape.gradient(loss, self.meta_model.trainable_variables)
+                # TODO check this alpha work or not
+                # element wise multiply alpha and gradients
+                grads = tf.math.multiply(grads, alpha)
                 inner_optimizer.apply_gradients(zip(grads, self.meta_model.trainable_variables))
 
             # save the weights after inner loop
             task_weights.append(self.meta_model.get_weights())
 
         with tf.GradientTape() as tape:
+            tape.watch(alpha)
+            tape.watch(self.meta_model.trainable_variables)
+            # calculate the test loss
             for i, (test_X, test_Y) in enumerate(zip(meta_test_X, meta_test_Y)):
                 # load the trained weight after inner loop
                 self.meta_model.set_weights(task_weights[i])
 
                 Y_hat = self.meta_model(test_X, training=True)
-                loss = losses.sparse_categorical_crossentropy(test_Y, Y_hat)
+                loss = self.loss(test_Y, Y_hat)
                 loss = tf.reduce_mean(loss)
                 batch_loss.append(loss)
-
             mean_loss = tf.reduce_mean(batch_loss)
 
         # start meta learning step: using the initialization weight
         self.meta_model.set_weights(meta_weights)
         if outer_optimizer:
-            grads = tape.gradient(mean_loss, self.meta_model.trainable_variables)
-            outer_optimizer.apply_gradients(zip(grads, self.meta_model.trainable_variables))
-
+            grads = tape.gradient(mean_loss, [self.meta_model.trainable_variables, alpha])
+            outer_optimizer.apply_gradients(zip(grads[0], self.meta_model.trainable_variables))
+            outer_optimizer.apply_gradients(zip(grads[1], alpha))
+            self.alpha = alpha
         return mean_loss
