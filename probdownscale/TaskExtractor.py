acc0c8e7751aa8b4389982644bd0348822026a83
WML <menglinw@usc.edu>
2022-04-23 23:29:41 -0700

    finished get a single task, and debug

--- probdownscale/TaskExtractor.py
+++ probdownscale/TaskExtractor.py
@@ -11,13 +11,95 @@ import sys
 import os
 import copy
 import pandas as pd
+from itertools import product
+from random import sample
+
 
 class TaskExtractor():
-    def __init__(self, data, n_task, test_proportion):
-        self.data = data
+    def __init__(self, data, lats_lons, task_dim, n_task, test_proportion, n_lag):
+        # initialize all arguments
+        # high and low resolution data
+        self.h_data, self.l_data = data
+        # high and low resolution latitude and longitude
+        self.h_lats, self.h_lons, self.l_lats, self.l_lons = lats_lons
+        # task dimension
+        if isinstance(task_dim, int):
+            self.task_dim = [task_dim, task_dim]
+        elif isinstance(task_dim, list) and len(task_dim)==2:
+            self.task_dim = task_dim
+        else:
+            raise ValueError
+        # number of task per meta batch
         self.n_task = n_task
+        # test proportion within each data
         self.test_proportion = test_proportion
+        # number of lagging days
+        self.n_lag = int(n_lag)
 
-    def draw_sample(self):
-        pass
+        # all available topleft index of tasks
+        avlb_lats = self.h_lats[:-(self.task_dim[0]-1)]
+        avlb_lons = self.h_lons[:-(self.task_dim[1]-1)]
+        self.avlb_location = list(product(avlb_lats, avlb_lons))
+        # collect all topleft index we have seen
+        self.seen_location = dict()
+
+    def _get_random_topleft_index(self, record=True):
+        '''
+        get a random topleft index of a task
+        :param record: True or False, record the task index to seen tasks
+        :return:
+        '''
+        sample_index = sample(self.avlb_location, 1)[0]
+        if record:
+            self.seen_location.setdefault(sample_index, 0)
+            self.seen_location[sample_index] += 1
+        return sample_index
+
+    def _get_random_task(self, record=True):
+        # get random topleft index
+        topleft_location = self._get_random_topleft_index(record=record)
+
+        # get high resolution data
+        lat_index = list(self.h_lats).index(topleft_location[0])
+        lon_index = list(self.h_lons).index(topleft_location[1])
+        h_data = self.h_data[:, lat_index:(lat_index+self.task_dim[0]), lon_index:(lon_index+self.task_dim[1])]
+
+        # get low resolution data
+        l_data = np.zeros_like(h_data)
+        for i, lat_idx in enumerate(range(lat_index, lat_index+self.task_dim[0])):
+            for j, lon_idx in enumerate(range(lon_index, (lon_index+self.task_dim[1]))):
+                lat = self.h_lats[lat_idx]
+                lon = self.h_lons[lon_idx]
+                l_lat_idx = np.argmin(np.abs(self.l_lats - lat))
+                l_lon_idx = np.argmin(np.abs(self.l_lons - lon))
+                l_data[:, i, j] = self.l_data[:, l_lat_idx, l_lon_idx]
+
+        # train test split
+        avlb_y = list(range(h_data.shape[0]))[self.n_lag:]
+        test_y_day = sample(avlb_y, int(len(avlb_y)*self.test_proportion))
+        train_y_day = list(set(avlb_y).difference(set(test_y_day)))
+
+        # flatten the data
+        # output dim (time, channel, rows, cols)
+        train_y = h_data[train_y_day]
+        test_y = h_data[test_y_day]
+
+        train_x_1, train_x_2, train_x_3 = self._get_inputX(train_y_day, h_data, l_data)
+        test_x_1, test_x_2, test_x_3 = self._get_inputX(test_y_day, h_data, l_data)
+        return [train_x_1, train_x_2, train_x_3], train_y, [test_x_1, test_x_2, test_x_3], test_y
+
+
+    def _get_inputX(self, y_index, h_data, l_data):
+        # input 1: HR temporal input
+        # input 2: LR image input
+        # input 3: day of the year
+        train_x_1 = np.zeros((len(y_index), self.n_lag, 1, self.task_dim[0], self.task_dim[1]))
+        for i, indx in enumerate(y_index):
+            train_x_1[i, :, :, :, :] = np.expand_dims(h_data[int(indx-self.n_lag):int(indx)], 1)
+        train_x_2 = l_data[y_index]
+        train_x_3 = np.remainder(np.array(y_index), 365)
+        return train_x_1, train_x_2, train_x_3
+
+
+
 
