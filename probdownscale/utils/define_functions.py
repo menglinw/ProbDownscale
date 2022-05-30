5d40c9b5e07d3f80b1e82451c7dbcf99ccf599d2
WML <menglinw@usc.edu>
2022-05-30 16:18:28 -0700

    adjust cov function parameter

--- probdownscale/utils/define_functions.py
+++ probdownscale/utils/define_functions.py
@@ -222,7 +222,7 @@ class run_metadownscale():
         start = time.time()
         # meta train with beta
         # define beta function
-        def covariance_function(h, phi=0.8):
+        def covariance_function(h, phi=5):
             return exp(-(h / phi)**2)
 
         def distance_function(loc1, loc2):
